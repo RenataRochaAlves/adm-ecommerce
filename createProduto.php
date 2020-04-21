9b0f6ae4314dd408d04230e136634b862e7db7b6
Renata Rocha Alves <renata.rocha.alves@usp.br>
2020-04-21 01:41:55 -0300

    adicionando o CSS da página de criação do produto

--- createProduto.php
+++ createProduto.php
@@ -21,30 +21,45 @@ if($_POST == false){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Adicionar Novo Produto | E-commerce</title>
 </head>
 <body>
     <header>
         <img src="img/e-commerce.png" alt="E-commerce Logo">
-        <ul>
-            <?php foreach($menu as $value): ?>
-            <li><a href="#"><?= $value ?></a></li>
-            <?php endforeach ?>
-        </ul>
+        <nav>
+            <ul>
+                <?php foreach($menu as $value): ?>
+                <li><a href="#"><?= $value ?></a></li>
+                <?php endforeach ?>
+            </ul>
+        </nav>
     </header>
 
     <main>
+    <h3>Adicionar Novo Produto</h3>
         <div id="form">
-            <h3>Adicionar Novo Produto</h3>
             <form action="createProduto.php" method="POST">
+
+                <div id="nome">
                 <label for="nomeProduto">Nome do Produto</label><br>
                     <input type="text" name="nomeProduto" id="nomeProduto" value="<?= $nome ?>" placeholder="Camiseta Arco-Íris" required><br>
+                </div>
+
+                <div id="descricao">
                 <label for="descricaoProduto">Descrição do Produto</label><br>
                     <textarea name="descricaoProduto" id="descricaoProduto" cols="30" rows="10" value="<?= $descricao ?>" placeholder="Camiseta com estampa localizada por silk screen de um arco-íris."><?= $descricao ?></textarea><br>
+                </div>
+
+                <div id="preco">
                 <label for="precoProduto">Preço do Produto</label><br>
                     <input type="number" step=0.01 name="precoProduto" id="precoProduto" value="<?= $preco ?>" placeholder="35,99" required><br>
+                </div>
+
+                <div id="foto">
                 <label for="fotoProduto">Imagem do Produto</label><br>
                     <input type="file" name="fotoProduto" id="fotoProduto" value="<?= $foto ?>" required><br>
+                </div>
 
                 <button type="submit">Enviar</button>
             </form>
